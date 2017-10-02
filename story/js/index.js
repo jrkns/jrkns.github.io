c499e1fbe0a154848734dc25bc33cb3e3bdfbdd5
jrkns <jrkns1996@gmail.com>
2017-10-02 23:32:26 +0700

    Test Again

--- story/js/index.js
+++ story/js/index.js
@@ -1,40 +1,32 @@
-$(function () {
-	var $content = $('#jsonContent');
-	var data = {
-		rss_url: 'https://medium.com/feed/@jrkns'
-	};
-	$.get('https://api.rss2json.com/v1/api.json', data, function (response) {
-		if (response.status == 'ok') {
-			var output = '';
-			$.each(response.items, function (k, item) {
-				var visibleSm;
-				if(k < 3){
-					visibleSm = '';
-				 } else {
-					 visibleSm = ' visible-sm';
-				 }
-				output += '<div class="col-sm-6 col-md-4' + visibleSm + '">';
-				output += '<div class="blog-post"><header>';
-				output += '<h4 class="date">' + $.format.date(item.pubDate, "dd<br>MMM") + "</h4>";
-				var tagIndex = item.description.indexOf('<img'); // Find where the img tag starts
-				var srcIndex = item.description.substring(tagIndex).indexOf('src=') + tagIndex; // Find where the src attribute starts
-				var srcStart = srcIndex + 5; // Find where the actual image URL starts; 5 for the length of 'src="'
-				var srcEnd = item.description.substring(srcStart).indexOf('"') + srcStart; // Find where the URL ends
-				var src = item.description.substring(srcStart, srcEnd); // Extract just the URL
-				output += '<div class="blog-element"><img class="img-responsive" src="' + src + '" width="360px" height="240px"></div></header>';
-				output += '<div class="blog-content"><h4><a href="'+ item.link + '">' + item.title + '</a></h4>';
+$(function() {
+    var $content = $('#jsonContent');
+    var data = {
+        rss_url: 'https://medium.com/feed/@jrkns'
+    };
+    $.get('https://api.rss2json.com/v1/api.json', data, function(response) {
+        if (response.status == 'ok') {
+            var output = '';
+            var posts = $.map(response.items, function(post, i) {
+                var postCategories = response.items[i].categories;
+                if (postCategories.length !== 0) {
+                    return post;
+                }
+            });
+            $.each(posts, function(k, item) {
+                var visibleSm;
+				output += '<div class="col-sm-12 col-md-12 visible-sm">';
+				output += '<div class="blog-post">';
+                var tagIndex = item.description.indexOf('<img');
+                var srcIndex = item.description.substring(tagIndex).indexOf('src=') + tagIndex;
+                var srcStart = srcIndex + 5;
+                var srcEnd = item.description.substring(srcStart).indexOf('"') + srcStart;
+                var src = item.description.substring(srcStart, srcEnd);
+				output += '<div class="blog-content"><img class="img-responsive" src="' + src + '" width="290px" height="162px"><br><h4><a href="'+ item.link + '">' + item.title + '</a></h4>';
 				output += '<div class="post-meta"><span>By ' + item.author + '</span></div>';
-				var yourString = item.description.replace(/<img[^>]*>/g,""); //replace with your string.
-				var maxLength = 120 // maximum number of characters to extract
-				//trim the string to the maximum length
-				var trimmedString = yourString.substr(0, maxLength);
-				//re-trim if we are in the middle of a word
-				trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")))
-				output += '<p>' + trimmedString + '...</p>';
-				output += '</div></div></div>';
-				return k < 3;
-			});
-			$content.html(output);
-		}
-	});
+                output += '</div></div></div>';
+                return true;
+            });
+            $content.html(output);
+        }
+    });
 });
