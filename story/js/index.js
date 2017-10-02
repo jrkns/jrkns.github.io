2d97aca069c4037c63dabe794edf6f8e2804657a
jrkns <jrkns1996@gmail.com>
2017-10-03 00:51:12 +0700

    Testing (11)

--- story/js/index.js
+++ story/js/index.js
@@ -22,7 +22,8 @@ $(function() {
                 var srcEnd = item.description.substring(srcStart).indexOf('"') + srcStart;
                 var src = item.description.substring(srcStart, srcEnd);
 				output += '<div class="blog-content"><center><a href="'+ item.link + '"><img class="img-responsive" src="' + src + '" width="480px" height="120px"></a><br><h4><a href="'+ item.link + '">' + item.title + '</a></h4></center>';
-                output += '</div></div></div>';
+                output += '<div class="post-meta"><span>By ' + item.author + ' @' + item.pubDate + '</span>';
+                output += '</div></div></div></div>';
                 return true;
             });
             $content.html(output);
