6da03a435b25befc8d02bfeeb2f6b036ef1faf1e
jrkns <jrkns1996@gmail.com>
2017-10-11 22:52:00 +0700

    Update Random

--- js/random.js
+++ js/random.js
@@ -1,9 +1,10 @@
-$(window).load.$(function() {
+$(window).load(function(){
 var output, started, duration, desired;
+
 
 // Constants
 duration = 5000;
-desired = '50';
+desired = Math.floor(Math.random() * 27) + 1;
 
 // Initial setup
 output = $('#output');
@@ -13,6 +14,7 @@ started = new Date().getTime();
 animationTimer = setInterval(function() {
     // If the value is what we want, stop animating
     // or if the duration has been exceeded, stop animating
+    // || new Date().getTime() - started > duration
     if (output.text().trim() === desired || new Date().getTime() - started > duration) {
         clearInterval(animationTimer);
     } else {
@@ -20,9 +22,9 @@ animationTimer = setInterval(function() {
         // Generate a random string to use for the next animation step
         output.text(
             ''+
-            Math.floor(Math.random() * 10)+
+            Math.floor(Math.random() * 2)+
             Math.floor(Math.random() * 10)
         );
     }
-}, 100);
+}, 50);
 });
